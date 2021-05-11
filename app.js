e82bb892ef9e9223f7707ede646e3fce112f0c26
Imri Neeman <imri@spearuav.com>
2021-05-11 19:38:06 +0300

    request query retrieval

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const morgan = require('morgan');
 const loginRouter = require('./controllers/loginRouter');
 
 const app = express();
+
 app.use(morgan('tiny'));
 app.use(loginRouter);
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>Hello</h1>');
+  res.status(200).send('<h1>Main Site</h1>');
+});
+
+app.get('/info', (req, res) => {
+  res.status(200).send('<h1>NEWAPP</h1>');
 });
 
 module.exports = app;
--- controllers/loginRouter.js
+++ controllers/loginRouter.js
@@ -3,14 +3,22 @@ const config = require('../utils/config');
 
 const baseURL = 'http://localhost:3005/';
 
+// Login and authorization for spotify API
+
 loginRouter.get('/login', (req, res) => {
-  const scopes = 'user-read-private user-read-email';
+  const scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-private user-follow-read user-follow-modify user-library-read';
 
   res.redirect(`${'https://accounts.spotify.com/authorize'
    + '?response_type=code'
-   + '&client_id='}${config.CLIENT_ID
-  }${scopes ? `&scope=${encodeURIComponent(scopes)}` : ''
-  }&redirect_uri=${encodeURIComponent(baseURL)}`);
+   + '&client_id='}
+   ${config.CLIENT_ID}
+   ${scopes ? `&scope=${encodeURIComponent(scopes)}` : ''
+}&redirect_uri=${encodeURIComponent(`${baseURL}callback`)}`);
+});
+
+loginRouter.get('/callback', (req, res) => {
+  console.log(req.query.code);
+  res.status(200).send('<h1>spotify callback</h1>');
 });
 
 module.exports = loginRouter;
